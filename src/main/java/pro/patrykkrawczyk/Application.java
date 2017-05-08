d484b172af8325f70540c5d8e699b96ec3b44c88
capybaracreations <capybaracreations@gmail.com>
2017-05-08 22:58:52 +0200

    initialization

---
+++ src/main/java/pro/patrykkrawczyk/Application.java
@@ -0,0 +1,23 @@
+package pro.patrykkrawczyk;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author Patryk Krawczyk
+ */
+@SpringBootApplication
+public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+
+}
